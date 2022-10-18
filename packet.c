e3e968385034bdb421809f10cc6986c2ca5fdc7e
danielmcnz <danielmcnz@gmail.com>
2022-10-18 14:30:00 +1300

    blocking issue fix continued

--- packet.c
+++ packet.c
@@ -7,8 +7,13 @@ uint8_t send_coords(packet_t *packet)
 {
     static uint8_t recv = 0;
 
-    if(recv == 0)
+    static uint8_t sent_coords = 0;
+
+    if(sent_coords == 0 && ir_uart_write_ready_p())
+    {
         ir_uart_putc(packet->coords.x + packet->coords.y * 10);
+        sent_coords = 1;
+    }
 
     if(ir_uart_read_ready_p())
     {
@@ -23,16 +28,13 @@ uint8_t recv_coords(packet_t *packet, uint8_t friendly_ships[])
 {
     static uint8_t recv = 0;
 
-    if(recv == 0)
+    // receive enemy attack coordinates
+    if(ir_uart_read_ready_p())
     {
-        // receive enemy attack coordinates
-        if(ir_uart_read_ready_p())
-        {
-            uint8_t in_val = ir_uart_getc();
-            packet->coords.x = in_val % 10;
-            packet->coords.y = (in_val - packet->coords.x) / 10;
-            recv = 1;
-        }
+        uint8_t in_val = ir_uart_getc();
+        packet->coords.x = in_val % 10;
+        packet->coords.y = (in_val - packet->coords.x) / 10;
+        recv = 1;
     }
 
     // send hit or miss dependent on coordinates
