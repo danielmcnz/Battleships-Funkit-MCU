af8d535fe6059d361ae887b4f23ffee36c4e610e
Chester Hall-Fernandez <cha219@cs18244kq.canterbury.ac.nz>
2022-10-18 13:37:31 +1300

    Fixing IR issues

--- packet.c
+++ packet.c
@@ -21,31 +21,30 @@ uint8_t send_coords(packet_t *packet)
 uint8_t recv_coords(packet_t *packet, uint8_t friendly_ships[])
 {
     recv = 0;
-
+    static uint8_t updated = 0;
     // receive enemy attack coordinates
-    if(ir_uart_read_ready_p())
+    if(ir_uart_read_ready_p() && updated != 1)
     {
         packet->coords.x = ir_uart_getc();
-        recv = 1;
+        updated = 1;
     }
-    if(ir_uart_write_ready_p())
+    if(ir_uart_read_ready_p() && updated == 1)
     {
         packet->coords.y = ir_uart_getc();
         recv = 1;
+        updated = 0;
     }
 
     // send hit or miss dependent on coordinates
-    for(uint8_t x=0;x<MAP_WIDTH; ++x)
-    {
-        for(uint8_t y=0; y<MAP_HEIGHT; ++y)
-        {
-            uint8_t result = friendly_ships[MAP_HEIGHT * x + (MAP_HEIGHT-1)-y];
-            if(result == 0)
-                result = 2;
-            packet->result = result;
-            ir_uart_putc(result);
-        }
+    if (recv == 1) {
+        uint8_t result = friendly_ships[MAP_HEIGHT * packet->coords.x + (MAP_HEIGHT-1)-packet->coords.y];
+        if(result == 0)
+            result = 2;
+        packet->result = result;
+        
+        ir_uart_putc(result);
     }
-
+    
+    
     return recv;
 }
