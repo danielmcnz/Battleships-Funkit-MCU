52f60575d20e705fcc446ace9d1774190d7e724d
Chester Hall-Fernandez <cha219@cs18219kq.canterbury.ac.nz>
2022-10-19 14:05:18 +1300

    More Commments yeet

--- map.c
+++ map.c
@@ -35,75 +35,24 @@ static uint8_t locations[N_SHIP_MAPS][35] =
 };
 
 
-void update_map(pos_t coords, uint8_t Map[], uint8_t RESULT){
+void update_map(pos_t coords, uint8_t Map[], uint8_t RESULT){ //Updates a given pixel position with the RESULT value 
     Map[MAP_HEIGHT * coords.x + (MAP_HEIGHT - 1) - coords.y] = RESULT;
 }
 
 
-void generate_ships(uint8_t map[35])
+void generate_ships(uint8_t map[35]) //Generates a random map
 {
-    // uint8_t direction = rand % 2; // horizontal = 0, vertical = 1
+    uint8_t *location = locations[rand() % 3]; //Gets the adress of a random map 
 
-    // // pos_t boats[] = { (1, 2), (1, 3),
-    // //                     (3, 2), (3, 3),
-    // //                     (2, 1), (3, 1), (4, 1)
-    // //                 };
-
-    // pos_t boats[] = {0};
-
-    // uint8_t pixel_count = 0;
-
-    // for(uint8_t n_boat = 0; n_boat < MAX_SHIPS; ++n_boat)
-    // {
-    //     for(uint8_t n_pixel = 1; n_pixel < BATTLESHIPS[n_boat]; ++n_pixel)
-    //     {
-    //         if(n_pixel == 0)
-    //         {
-    //             if(direction == 0)
-    //             {
-    //                 boats[pixel_count + n_pixel].x = rand % (MAP_HEIGHT + 1 - BATTLESHIPS[n_boat]);
-    //                 boats[pixel_count + n_pixel].y = rand % (MAP_WIDTH + 1);
-    //             }
-    //             else
-    //             {
-    //                 boats[pixel_count + n_pixel].x = rand % (MAP_WIDTH + 1);
-    //                 boats[pixel_count + n_pixel].y = rand % (MAP_HEIGHT + 1 - BATTLESHIPS[n_boat]);
-    //             }
-    //         }
-    //         else
-    //         {
-    //             if(direction == 0)
-    //             {
-    //                 boats[pixel_count + n_pixel].x = boats[pixel_count + n_pixel-1].x++;
-    //             }
-    //             else
-    //             {
-    //                 boats[pixel_count + n_pixel].y = boats[pixel_count + n_pixel-1].y++;
-    //             }
-    //         }
-
-    //         ++pixel_count;
-    //     }
-
-        
-    // }
-
-    // fill map
-
-
-
-
-    uint8_t *location = locations[rand() % 3];
-
-    // fill map
-    for (uint8_t i=0; i<MAP_WIDTH * MAP_HEIGHT; ++i)
+    
+    for (uint8_t i=0; i<MAP_WIDTH * MAP_HEIGHT; ++i) // fills map
     {
         map[i] = location[i];
     }
 }
 
 
-uint8_t get_position_value(uint8_t map[], pos_t position)
+uint8_t get_position_value(uint8_t map[], pos_t position) //returns the value of a pixel at a position 
 {
     return map[MAP_HEIGHT * position.x + (MAP_HEIGHT - 1 ) - position.y];
 }
